4d4cce01e46b3e66eded6735901b111d76955580
bearcub21 <william.javier.gallardo@outlook.com>
2015-11-14 09:03:05 -0400

    Nombre y Apellido

---
+++ Nombre y Apellido/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//William Gallardo - 14-2943
+namespace Nombre_y_Apellido
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string Nombre, Apellido;
+			Console.WriteLine("Hola! Mi Nombre es Jarvis. Cual es tu nombre?");
+			Nombre= Console.ReadLine();
+			Console.WriteLine("Hmmm... Y Cual es tu apellido, " + Nombre + "?");
+			Apellido= Console.ReadLine();
+			Console.Write("Mucho Gusto, " + Nombre + " " + Apellido + "! Tomaremos el té.");
+			Console.ReadKey(true);
+		}
+	}
+}
